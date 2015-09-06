7687ca3f8621bbb597fdc305decb5dcad712e2cd
Clara Raffin <clararaffin@users.noreply.github.com>
2015-09-06 18:58:39 -0300

    Update emr.py

--- emr.py
+++ emr.py
@@ -13,4 +13,29 @@ Existen dos tipos de tarjeta:
 TarjetaComun: Paga el boleto a 5.75 y el transbordo a 1.90
 TarjetaMedioBoleto: Paga el boleto a 2.90 y el transbordo a 0,96
 """
+from datetime import datetime, timedelta, time
+now = datetime.now()
 
+class Card:
+	def __init__(self):
+		self.saldo = 0
+		self.busprev = 0
+		self.horabusprev = 0
+		self.flagbondiprev = False
+		
+		def Boleto():
+		  #normal 5.75
+		  #transbordo 1.90
+		
+		#recargar
+		#historial
+		#saldo
+
+class Medio(Card):
+  #normal 2.90
+  #transbordo 0.96
+  
+class Viajes:
+  #datos del viaje
+class Bondi:
+  #datos del bondi
