66e3cb5502d2ca978feb50bb9e7ec2b6250dce41
Clara Raffin <clararaffin@users.noreply.github.com>
2015-09-08 01:49:46 -0300

    Update emr.py

--- emr.py
+++ emr.py
@@ -1,19 +1,3 @@
-"""
-El EMR (Ente de Movilidad de Rosario) está interesado en modernizar su software de gestión de tarjetas
-magnéticas. En esta primera etapa están buscando construir un software que permita simular el funcionamiento de
-una tarjeta magnética en los siguientes escenarios:
-
-1) Tarjeta.PagarBoleto(colectivo, horario)
-2) Tarjeta.Recarga(monto)
-3) Tarjeta.Saldo()
-4) Tarjeta.ViajesRealizados()
-
-Considerar las siguientes restricciones
-Existen dos tipos de tarjeta:
-TarjetaComun: Paga el boleto a 5.75 y el transbordo a 1.90
-TarjetaMedioBoleto: Paga el boleto a 2.90 y el transbordo a 0,96
-"""
-
 from datetime import timedelta, datetime
 
 class Colectivo:
