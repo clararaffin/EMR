20ce37243a0e3f0a1b49bbd7b723aa64decf50fa
Clara Raffin <clararaffin@users.noreply.github.com>
2015-09-08 16:47:54 -0300

    Update emr.py

--- emr.py
+++ emr.py
@@ -68,7 +68,7 @@ class Medio(Card):
   	self.hora=datetime.strptime (hora, "%d/%m/%Y %H:%M")
   	if self.hora.time().hour >= 6 and self.hora.time().hour <= 0:
 	  	if self.busprev!=colectivo.linea and self.flagbondiprev==True and self.hora-self.horabusprev < timedelta(hour=1):
-			if self.saldo >= 0.96:
+	  		if self.saldo >= 0.96:
 				self.saldo=self.saldo-0.96
 				self.busprev=0
 				self.horabusprev=0
