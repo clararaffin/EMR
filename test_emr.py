cc5303a5381b7886b5b6857a592d129876a72338
Clara Raffin <clararaffin@users.noreply.github.com>
2015-09-08 16:13:26 -0300

    Update test_emr.py

--- test_emr.py
+++ test_emr.py
@@ -5,16 +5,16 @@ Tarjeta=Card()
 Medio=Medio()
 def test1():
   Tarjeta.cargar(196)
-  assert Tarjeta.saldo() == 230
+  assert (Tarjeta.getSaldo() == 230)
   Medio.cargar(368)
-  assert Medio.saldo() == 460
+  assert (Medio.getSaldo() == 460)
 def test2():
   Tarjeta.boleto(N142,"08/09/2015 00:45")
   Tarjeta.boleto(N116,"08/09/2015 00:55")
-  assert Tarjeta.saldo() == 222.35
+  assert Tarjeta.getSaldo() == 222.35
   Medio.boleto(N142,"08/09/2015 12:00")
   Medio.boleto(N116,"08/09/2015 12:10")
-  assert Medio.saldo() == 456.14
+  assert Medio.getSaldo() == 456.14
   Medio.boleto(N142,"08/09/2015 02:30")
   Medio.boleto(N116,"08/09/2015 02:50")
-  assert Medio.saldo() == 448.39
+  assert Medio.getSaldo() == 448.39
