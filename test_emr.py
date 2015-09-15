1217990e6f0653139f6c6f025e9e5e827f658428
Clara Raffin <clararaffin@users.noreply.github.com>
2015-09-14 23:59:59 -0300

    Update test_emr.py

--- test_emr.py
+++ test_emr.py
@@ -17,13 +17,20 @@ def test_cargarMedio():
   Medio.cargar(368)
   assert Medio.getSaldo() == 460
 
-def test_boleto():
+def test_boletoNormal():
+  #con transbordo
   Tarjeta.boleto(N142,"08/09/2015 00:45")
   Tarjeta.boleto(N116,"08/09/2015 00:55")
   assert Tarjeta.getSaldo() == 222.35
+
+def test_medio():
+  #con transbordo
   Medio.boleto(N142,"08/09/2015 12:00")
   Medio.boleto(N116,"08/09/2015 12:10")
   assert Medio.getSaldo() == 456.14
+
+def test_medioOff():
+  #medio boleto fuera de horario con transbordo
   Medio.boleto(N142,"08/09/2015 02:30")
   Medio.boleto(N116,"08/09/2015 02:50")
   assert Medio.getSaldo() == 448.49
