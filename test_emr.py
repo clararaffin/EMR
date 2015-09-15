5e1951c8a9157ab486ae2f23e04d2d7a819f15d9
Clara Raffin <clararaffin@users.noreply.github.com>
2015-09-15 00:45:57 -0300

    Update test_emr.py

--- test_emr.py
+++ test_emr.py
@@ -49,19 +49,18 @@ def test_medioSinsaldo():
 
 def test_listaViajesNormal():
   Tarjeta3 = Card()
-	Tarjeta3.cargar(100)
-	Tarjeta3.auxboleto(N116, "08/09/2015 15:30")
-	Tarjeta3.auxboleto(N142, "08/09/2015 15:40")
-	viajes = Tarjeta3.historial
-
-	assert viajes[0].hora == "08/09/2015 15:30"
-	assert viajes[0].monto == 5.75
-	assert viajes[0].empresa == "Semtur"
-	assert viajes[0].linea == 116
-	assert viajes[0].interno == 1
+  Tarjeta3.cargar(100)
+  Tarjeta3.auxboleto(N116, "08/09/2015 15:30")
+  Tarjeta3.auxboleto(N142, "08/09/2015 15:40")
+  viajes = Tarjeta3.historial
   
+  assert viajes[0].hora == "08/09/2015 15:30"
+  assert viajes[0].monto == 5.75
+  assert viajes[0].empresa == "Semtur"
+  assert viajes[0].linea == 116
+  assert viajes[0].interno == 1
   assert viajes[1].hora == "08/09/2015 15:40"
-	assert viajes[1].monto == 1.9
-	assert viajes[1].empresa == "Rosario Bus"
-	assert viajes[1].linea == 142
-	assert viajes[1].interno == 3
+  assert viajes[1].monto == 1.9
+  assert viajes[1].empresa == "Rosario Bus"
+  assert viajes[1].linea == 142
+  assert viajes[1].interno == 3
